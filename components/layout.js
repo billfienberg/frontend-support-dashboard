7e4f352c7ca34e74afd3c168a444ac3f23dfee04
Bill Fienberg <billfienberg@gmail.com>
2021-02-15 14:38:23 -0600

    Update siteTitle to 'Frontend Support Dashboard for VA.gov'

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import styles from "./layout.module.css"
 import utilStyles from "../styles/utils.module.css"
 import Link from "next/link"
 
-export const siteTitle = "Next.js Sample Website"
+export const siteTitle = "Frontend Support Dashboard for VA.gov"
 
 export default function Layout({ children, home }) {
   return (
