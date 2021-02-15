d7246717c7834e21834a45c31e516596899a9f48
Bill Fienberg <billfienberg@gmail.com>
2021-02-15 15:25:45 -0600

    Update head and layout components

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,7 @@
 import Head from "next/head"
-import styles from "./layout.module.css"
-import utilStyles from "../styles/utils.module.css"
 import Link from "next/link"
 
-export const siteTitle = "Next.js Sample Website"
+export const siteTitle = "Frontend Support Dashboard for VA.gov"
 
 export default function Layout({ children, home }) {
   return (
